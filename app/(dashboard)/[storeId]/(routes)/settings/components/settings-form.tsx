04132542f74d42307050afa4c386ad92ee7b3766
Pepui <lujerkovic@gmail.com>
2023-07-07 13:03:17 +0200

    change to settings-form

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -2,17 +2,16 @@
 
 import * as z from 'zod';
 import axios from 'axios';
-import { useState } from 'react';
-import { Store } from '@prisma/client';
-import { Trash } from 'lucide-react';
-import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
 import { toast } from 'react-hot-toast';
+import { Trash } from 'lucide-react';
+import { Store } from '@prisma/client';
 import { useParams, useRouter } from 'next/navigation';
+import { useState } from 'react';
 
-import { Heading } from '@/components/ui/heading';
+import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
-import { Separator } from '@/components/ui/separator';
 import {
   Form,
   FormControl,
@@ -21,20 +20,21 @@ import {
   FormLabel,
   FormMessage,
 } from '@/components/ui/form';
-import { Input } from '@/components/ui/input';
+import { Separator } from '@/components/ui/separator';
+import { Heading } from '@/components/ui/heading';
 import { AlertModal } from '@/components/modals/alert-modal';
 import { ApiAlert } from '@/components/ui/api-alert';
 import { useOrigin } from '@/hooks/use-origin';
 
-interface SettingsFormProps {
-  initialData: Store;
-}
-
 const formSchema = z.object({
-  name: z.string().min(1),
+  name: z.string().min(2),
 });
 
 type SettingsFormValues = z.infer<typeof formSchema>;
+
+interface SettingsFormProps {
+  initialData: Store;
+}
 
 export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
   const params = useParams();
@@ -54,9 +54,9 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
       setLoading(true);
       await axios.patch(`/api/stores/${params.storeId}`, data);
       router.refresh();
-      toast.success('Store updated successfully');
-    } catch (error) {
-      toast.error('Something went wrong');
+      toast.success('Store updated.');
+    } catch (error: any) {
+      toast.error('Something went wrong.');
     } finally {
       setLoading(false);
     }
@@ -68,9 +68,9 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
       await axios.delete(`/api/stores/${params.storeId}`);
       router.refresh();
       router.push('/');
-      toast.success('Store deleted successfully');
-    } catch (error) {
-      toast.error('Make sure you removed all products and categories first');
+      toast.success('Store deleted.');
+    } catch (error: any) {
+      toast.error('Make sure you removed all products and categories first.');
     } finally {
       setLoading(false);
       setOpen(false);
@@ -86,11 +86,14 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
         loading={loading}
       />
       <div className="flex items-center justify-between">
-        <Heading title="Settings" description="Manage store preferences" />
+        <Heading
+          title="Store settings"
+          description="Manage store preferences"
+        />
         <Button
           disabled={loading}
           variant="destructive"
-          size="icon"
+          size="sm"
           onClick={() => setOpen(true)}
         >
           <Trash className="h-4 w-4" />
@@ -102,7 +105,7 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
           onSubmit={form.handleSubmit(onSubmit)}
           className="space-y-8 w-full"
         >
-          <div className="grid grid-col-3 gap-8">
+          <div className="grid grid-cols-3 gap-8">
             <FormField
               control={form.control}
               name="name"
@@ -129,6 +132,7 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
       <Separator />
       <ApiAlert
         title="NEXT_PUBLIC_API_URL"
+        variant="public"
         description={`${origin}/api/${params.storeId}`}
       />
     </>
